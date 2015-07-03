201bfe4d33b2149f19d314aaaa3b55a2ccb5ddf0
Aleksey Zhidkov <aleksey.zhidkov@gmail.co>
2015-07-03 22:30:36 +0600

    Chapter 2, Exc 5: implement fun <A, B, C> uncurry(f: (A) -> (B) -> C): (A, B) -> C = { a: A, b: B -> f(a)(b) }

--- src/main/kotlin/ch2/ex5-uncurry.kt
+++ src/main/kotlin/ch2/ex5-uncurry.kt
@@ -1,2 +1,13 @@
-package ch2
+package ch2.uncurry
+
+import ch2.curry.curry
+
+fun <A, B, C> uncurry(f: (A) -> (B) -> C): (A, B) -> C = { a: A, b: B -> f(a)(b) }
+
+fun main(args: Array<String>) {
+    val add = { a:Int, b: Int -> a + b}
+    val addCurried = curry(add)
+    val addUncurried = uncurry(addCurried)
+    assert(4 == addUncurried(2, 2))
+}
 
